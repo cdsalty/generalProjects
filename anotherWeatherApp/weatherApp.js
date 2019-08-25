51774d25a07403c7e350946afebe8710d61e97e9
cdsalty <soltiscd@gmail.com>
2019-08-24 23:50:33 -0400

    ready for api

--- anotherWeatherApp/weatherApp.js
+++ anotherWeatherApp/weatherApp.js
@@ -10,3 +10,22 @@ iconElement.innerHTML = `<image src = "icons/${weather.iconId}.png"/>`;
 tempatureElement.innerHTML = `${weather.teampature.value}º <span>C</span>`;
 tempatureDescrip.innerHTML = weather.description;
 locationElement.innerHTML = `${weather.city}, ${weather.state}`;
+
+tempatureElement.addEventListener("click", function () {
+  if(weather.tempature.unit === undefined) return;  // No data, no go
+  if(weather.tempature.unit === "celsius") {
+    let fahrenheit = celsiusToFahrenheit(weather.tempature.value);
+    fahrenheit = Math.floor(fahrenheit); 
+    tempatureElement.innerHTML = `${fahrenheit}º <span>F</span>`
+    weather.tempature.unit = "fahrenheit";
+  }else{
+    tempatureElement.innerHTML = `${weather.tempature.value}º <span>C</span>`;
+    weather.tempature.unit = "celsius";
+  }
+});
+
+weather.tempature.value = 300 - 273
+
+function celsiusToFahrenheit(tempature) {
+  return (tempature * 9/5) + 32;
+}
