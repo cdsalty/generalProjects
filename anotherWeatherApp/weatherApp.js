a30b5ba156f2a20be4dacc2ec3355dd8e3bde18e
cdsalty <soltiscd@gmail.com>
2019-08-24 23:18:24 -0400

    removed files, defined innerHTML

--- anotherWeatherApp/weatherApp.js
+++ anotherWeatherApp/weatherApp.js
@@ -4,6 +4,9 @@ const notificationElement = document.querySelector(".notification");
 const iconElement = document.querySelector(".weather-icon");
 const tempatureElement = document.querySelector(".temperature-value p"); // assigning a child of a parent
 const tempatureDescrip = document.querySelector(".temperature-description p");
-const location = document.querySelector(".location p");
+const locationElement = document.querySelector(".location p");
 
-iconElement.innerHTML = 
+iconElement.innerHTML = `<image src = "icons/${weather.iconId}.png"/>`;
+tempatureElement.innerHTML = `${weather.teampature.value}º <span>C</span>`;
+tempatureDescrip.innerHTML = weather.description;
+locationElement.innerHTML = `${weather.city}, ${weather.state}`;
