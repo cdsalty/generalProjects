c5dedf9d2db3362bf7158422132fc16a7c56f429
cdsalty <soltiscd@gmail.com>
2019-08-24 22:14:55 -0400

    initial commit

---
+++ anotherWeatherApp/weatherApp.js
@@ -0,0 +1,9 @@
+// alert("sanity-check");
+
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempatureElement = document.querySelector(".temperature-value p"); // assigning a child of a parent
+const tempatureDescrip = document.querySelector(".temperature-description p");
+const location = document.querySelector(".location p");
+
+iconElement.innerHTML = 
