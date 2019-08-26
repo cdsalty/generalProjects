335fa64887b7a1c402e99d12696600c7ad55d3f0
cdsalty <soltiscd@gmail.com>
2019-08-25 22:57:23 -0400

    next to determine location and connect api

--- anotherWeatherApp/weatherApp.js
+++ anotherWeatherApp/weatherApp.js
@@ -1,25 +1,44 @@
 // alert("sanity-check");
 
-const notificationElement = document.querySelector(".notification");
-const iconElement = document.querySelector(".weather-icon");
-const tempatureElement = document.querySelector(".temperature-value p"); // assigning a child of a parent
-const tempatureDescrip = document.querySelector(".temperature-description p");
-const locationElement = document.querySelector(".location p");
 
+// SELECT ELEMENTS
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p"); // assigning a child of a parent
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+// The APP DATA
+const weather = {};
+
+weather.tempature = {
+  unit: "celsius"
+}
+
+// APP CONSTS AND VARS
+const KELVIN = 273; // SUBTRACT 300 FOR C
+// API KEY
+const key = "82005d27a116c2880c8f0fcb866998a0";
+
+// VERIFY THE USER'S BROWSER PROVIDES GEOLOCATION
+
+
+
+// THE PROCESS AND LOGIC:
 iconElement.innerHTML = `<image src = "icons/${weather.iconId}.png"/>`;
-tempatureElement.innerHTML = `${weather.teampature.value}º <span>C</span>`;
-tempatureDescrip.innerHTML = weather.description;
+tempElement.innerHTML = `${weather.teampature.value}º <span>C</span>`;
+descElement.innerHTML = weather.description;
 locationElement.innerHTML = `${weather.city}, ${weather.state}`;
 
-tempatureElement.addEventListener("click", function () {
+tempElement.addEventListener("click", function () {
   if(weather.tempature.unit === undefined) return;  // No data, no go
   if(weather.tempature.unit === "celsius") {
     let fahrenheit = celsiusToFahrenheit(weather.tempature.value);
     fahrenheit = Math.floor(fahrenheit); 
-    tempatureElement.innerHTML = `${fahrenheit}º <span>F</span>`
+    tempElement.innerHTML = `${fahrenheit}º <span>F</span>`
     weather.tempature.unit = "fahrenheit";
   }else{
-    tempatureElement.innerHTML = `${weather.tempature.value}º <span>C</span>`;
+    tempElement.innerHTML = `${weather.tempature.value}º <span>C</span>`;
     weather.tempature.unit = "celsius";
   }
 });
